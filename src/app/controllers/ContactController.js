a7fea8a4f27dddf96d3ff2125cc63705f656c0cb
Marcelo Siqueira <marcelojobxt7@gmail.com>
2023-11-28 19:10:03 -0300

    feat: Created routes of delete and show

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,14 +1,22 @@
 const ContactsRepository = require('../repositories/ContactsRepository')
 
 class ContactController {
-  index(request, response) {
+  async index(request, response) {
     // Listar todos os registros
-    const contacts = ContactsRepository.findAll()
-    response.json(contacts)
+    const contacts = await ContactsRepository.findAll()
+    response.json(contacts);
   }
 
-  show() {
+  async show(request, response) {
     // Obter um registro
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
+
+    if(!contact) {
+      response.status(404).json({error: 'User not found!'})
+    }
+
+    response.json(contact)
   }
 
   store() {
@@ -19,8 +27,17 @@ class ContactController {
     //Editar um registro
   }
   
-  delete() {
+  async delete(request, response) {
     //Deletar um registro
+    const { id } = request.params
+    const contact = await ContactsRepository.findById(id)
+
+    if(!contact) {
+      response.status(404).json({error: 'User not found!'});
+    }
+
+    await ContactsRepository.delete(id);
+    response.status(200).json({sucess: ` The user ${contact.name} was deleted`})
   }
 }
 module.exports = new ContactController()
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,54 @@
-const { uuid } = require('uuidv4')
-const contacts = [
+const { v4 } = require('uuid');
+
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Marcelo',
     email: 'Marcelojobxt7@gmail.com',
     phone: '5527988596066',
-    category_id: uuid(),
+    category_id: v4(),
   },
-]
+  {
+    id: v4(),
+    name: 'Luciana',
+    email: 'Luciana115@gmail.com',
+    phone: '5527988596066',
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Juliana',
+    email: 'Juliana720@gmail.com',
+    phone: '5527988596066',
+    category_id: v4(),
+  },
+];
 
 
 class ContactsRepository {
   findAll() {
-    return contacts
+    return new Promise((resolve) => { resolve(contacts) })
   }
-}
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id)
+    ));
+  };
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id)
+      resolve()
+    });
+  }
+};
 
 module.exports = new ContactsRepository();
 
 // Uso do singleton
 // Aqui terá todas as regras de manipulação do banco de dados, 
-// Usa-se uma padronização de nomes dos metodos como: findAll => buscar tudo  
+// Usa-se uma padronização de nomes dos metodos como: findAll => buscar tudo 
+
+//Dentro das funções do meu Repository Pattern, nós nunca teremos uma regra de negócio (ou seja, um if/else)
+//Sua única função é acessar o Data Source
