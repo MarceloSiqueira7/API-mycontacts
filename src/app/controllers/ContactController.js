6e094690b731ac5bfb1125038d116ff9cba2a16b
Marcelo Siqueira <marcelojobxt7@gmail.com>
2023-12-11 16:44:02 -0300

    Revert "Creating the user editing route and refactoring the code"
    This reverts commit 0d820d260e50243adf212827564be5708736752c.

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,82 +1,63 @@
 const ContactsRepository = require('../repositories/ContactsRepository');
 
 class ContactController {
-  // Listar todos os registros
   async index(request, response) {
+    // Listar todos os registros
     const contacts = await ContactsRepository.findAll();
     response.json(contacts);
   }
 
-  // Obter um registro
   async show(request, response) {
+    // Obter um registro
     const { id } = request.params;
-  
-    if(!(await ContactsRepository.findById(id))) {
+    const contact = await ContactsRepository.findById(id);
+
+    if(!contact) {
       // 404: Not found
       return response.status(404).json({error: 'User not found!'});
-    };
-    response.json(contact);
-  };
+    }
 
-  //Criar um registro
+    response.json(contact);
+  }
+
   async store(request, response) {
+    //Criar um registro
     const {name, email, phone, category_id} = request.body;
 
-    const [nameExist, emailExist] = await Promise.all([ ContactsRepository.findByName(name), ContactsRepository.findByEmail(email) ]);
-    
-    if(!name) {
-      return response.status(400).json({error: "Name is required"});
-    };
+    const [nameExist, emailExist] = await Promise.all([ ContactsRepository.findByName(name), ContactsRepository.findByEmail(email) ])
+
     if(nameExist) {
-      return response.status(400).json({error: "This name is already been in use"});
+      return response.status(400).json({error: "This name is already been taken"});
     };
 
     if(emailExist) {
-      return response.status(400).json({error: "This email is already been in use "});
-    };
+      return response.status(400).json({error: "This email is already been taken "})
+    }
 
     await ContactsRepository.create({ name, email, phone, category_id });
-    response.json({sucess: `User ${name} was created!`});
-  };
-
-  //Editar um registro
-  async update(request, response) {
-    
-    const { id } = request.params;
-    const {name, email, phone, category_id} = request.body;
-    
-    if(!(await ContactsRepository.findById(id))) {
-      return response.status(404).json({error: "User not found"});
-    };
-
-    if(!name) {
-      return response.status(404).json({error: "Name is required!"});
-    };
-
-    const emailExist = await ContactsRepository.findByEmail(email);
-
-    if(emailExist && emailExist.id !== id) {
-      return response.status(404).json({error: "This email is already in use"});
-    };
+    response.json({sucess: `User ${name} was created!`})
 
 
-    const contact = await ContactsRepository.update(id, { name, email, phone, category_id} );
-    response.json(contact);
-  };
+  }
+
+  update() {
+    //Editar um registro
+  }
   
-  //Deletar um registro
   async delete(request, response) {
-    const { id } = request.params;
-    
-    if(!(await ContactsRepository.findById(id))) {
+    //Deletar um registro
+    const { id } = request.params
+    const contact = await ContactsRepository.findById(id)
+
+    if(!contact) {
       response.status(404).json({error: 'User not found!'});
     }
 
     await ContactsRepository.delete(id);
-    response.status(200).json({sucess: ` The user ${contact.name} was deleted`});
+    response.status(200).json({sucess: ` The user ${contact.name} was deleted`})
   }
 }
-module.exports = new ContactController();
+module.exports = new ContactController()
 
 //singleton
 // Aqui fica toda nossa regra de negócio da aplicação, onde buscamos um registro, editamos, deletamos e etc
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
 // Aqui configuramos todas as rotas que nossa aplicação irá ter.
 const { Router } = require('express');
-const router = Router();
+const router = Router()
 const ContactController = require('./app/controllers/ContactController');
 
 router.get('/contacts', ContactController.index);
-router.get('/contacts/:id', ContactController.show);
-router.delete('/contacts/:id', ContactController.delete);
-router.post('/contacts', ContactController.store);
-router.put('/contacts/:id', ContactController.update);
+router.get('/contacts/:id', ContactController.show)
+router.delete('/contacts/:id', ContactController.delete)
+router.post('/contacts', ContactController.store)
 module.exports = router;
 
 
